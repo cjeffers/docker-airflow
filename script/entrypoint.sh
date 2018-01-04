22f469828389de4747a1e41ac06e28455820a4b9
cjeffers <cwjeffers18@gmail.com>
2018-01-03 16:10:15 -0800

    allow rabbitmq as a celery broker

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,6 +2,9 @@
 
 TRY_LOOP="20"
 
+: "${CELERY_BROKER:="Redis"}"
+: "${CELERY_RESULT_BACKEND:="Postgres"}"
+
 : "${REDIS_HOST:="redis"}"
 : "${REDIS_PORT:="6379"}"
 : "${REDIS_PASSWORD:=""}"
@@ -12,9 +15,16 @@ TRY_LOOP="20"
 : "${POSTGRES_PASSWORD:="airflow"}"
 : "${POSTGRES_DB:="airflow"}"
 
+: "${RABBITMQ_HOST:="rabbitmq"}"
+: "${RABBITMQ_PORT:="5672"}"
+: "${RABBITMQ_USER:="airflow"}"
+: "${RABBITMQ_PASSWORD:="airflow"}"
+: "${RABBITMQ_VHOST:="airflow"}"
+
 # Defaults and back-compat
 : "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
 : "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}"
+: "${AIRFLOW__WEBSERVER__SECRET_KEY}:=${AIRFLOW_FLASK_KEY:=temporary_key}"
 
 export \
   AIRFLOW__CELERY__BROKER_URL \
@@ -31,7 +41,7 @@ then
   AIRFLOW__CORE__LOAD_EXAMPLES=False
 fi
 
-# Install custome python package if requirements.txt is present
+# Install custom python package if requirements.txt is present
 if [ -e "/requirements.txt" ]; then
     $(which pip) install --user -r /requirements.txt
 fi
@@ -56,22 +66,61 @@ wait_for_port() {
   done
 }
 
-wait_for_redis() {
-  # Wait for Redis iff we are using it
+wait_for_celery_broker() {
   if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]
   then
-    wait_for_port "Redis" "$REDIS_HOST" "$REDIS_PORT"
+    case "$CELERY_BROKER" in
+      Redis)
+        wait_for_port "Redis" "$REDIS_HOST" "$REDIS_PORT"
+        ;;
+      RabbitMQ)
+        wait_for_port "RabbitMQ" "$RABBITMQ_HOST" "$RABBITMQ_PORT"
+        ;;
+      *)
+        echo >&2 "$(date) - unrecognized celery broker: $CELERY_BROKER"
+        exit 1
+    esac
   fi
 }
 
+get_celery_broker_url() {
+  case "$CELERY_BROKER" in
+    Redis)
+      return "redis://$REDIS_PREFIX$REDIS_HOST:$REDIS_PORT/1"
+      ;;
+    RabbitMQ)
+      return "amqp://$RABBITMQ_USER:$RABBITMQ_PASSWORD@$RABBITMQ_HOST:$RABBITMQ_PORT/$RABBITMQ_VHOST"
+      ;;
+    *)
+      echo >&2 "$(date) - unrecognized celery broker: $CELERY_BROKER"
+      exit 1
+      ;;
+  esac
+}
+
+get_celery_result_backend() {
+  case "$CELERY_RESULT_BACKEND" in
+    Postgres)
+      return "db+postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
+      ;;
+    RabbitMQ)
+      return "amqp://$RABBITMQ_USER:$RABBITMQ_PASS@$RABBITMQ_HOST:$RABBITMQ_PORT/$RABBITMQ_VHOST"
+      ;;
+    *)
+      echo >&2 "$(date) - unrecognized celery result backend: $CELERY_RESULT_BACKEND"
+      exit 1
+      ;;
+  esac
+}
+
 AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
-AIRFLOW__CELERY__BROKER_URL="redis://$REDIS_PREFIX$REDIS_HOST:$REDIS_PORT/1"
-AIRFLOW__CELERY__CELERY_RESULT_BACKEND="db+postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
+AIRFLOW__CELERY__BROKER_URL=$(get_celery_broker_url)
+AIRFLOW__CELERY__CELERY_RESULT_BACKEND=$(get_celery_result_backend)
 
 case "$1" in
   webserver)
     wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
-    wait_for_redis
+    wait_for_celery_broker
     airflow initdb
     if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ];
     then
@@ -82,13 +131,13 @@ case "$1" in
     ;;
   worker|scheduler)
     wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
-    wait_for_redis
+    wait_for_celery_broker
     # To give the webserver time to run initdb.
     sleep 10
     exec airflow "$@"
     ;;
   flower)
-    wait_for_redis
+    wait_for_celery_broker
     exec airflow "$@"
     ;;
   version)
